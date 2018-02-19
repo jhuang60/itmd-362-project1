3fa1ce590a09c4e1325a8f65a1a14b593517d384
Jiayu Huang <jhuang60@hawk.iit.edu>
2018-02-18 18:14:22 -0600

    function for name validation

--- site.js
+++ site.js
@@ -2,4 +2,13 @@
   $('#inputform').on('submit', function(e) {
     e.preventDefault();
 
+//name validation
+function namevalidation(name) {
+  var result = false;
+  if (name.length > 0) {
+    result = true;
+  }
+  return result;
+}
+
 })(jQuery);
